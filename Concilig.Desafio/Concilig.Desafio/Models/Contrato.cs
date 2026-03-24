76616ef5a6f69b59313180ca05eac51d2cebc5e4
Nathan Victor Esposito <nvictoresposito@gmail.com>
2026-03-24 11:08:28 -0300

    Create core data models and import contracts

---
+++ Concilig.Desafio/Concilig.Desafio/Models/Contrato.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Modelo do Contrato
+public class Contrato
+{
+    // Itens do Contrato
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string NumeroContrato { get; set; }
+
+    [Required]
+    public string Cliente { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Valor { get; set; }
+
+    public DateTime DataVencimento { get; set; }
+
+    // Relacionamento
+    public int ImportacaoId { get; set; }
+    public Importacao importacao { get; set; }
+}
---
+++ Concilig.Desafio/Concilig.Desafio/Models/Importacao.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+// Modelo de Importacao
+public class Importacao
+{
+    // Itens de Importacao
+    [Key]
+    public int Id { get; set; }
+
+    public string NomeArquivo { get; set; }
+
+    public DateTime DataImportacao { get; set; }
+
+    public string UsuarioId { get; set; }
+
+    public int QuantidadeRegistros { get; set; }
+
+    // Relacionamento
+    public List<Contrato> Contratos { get; set; }
+}
